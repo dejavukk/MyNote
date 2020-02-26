c521087ea1b8f8e4b97d1e910851050f570ba746
junhyuk <dejavukk1@gmail.com>
2020-02-26 23:47:46 +0900

    테이블뷰 cellForRowAt 메소드 구현

--- MyNote/Controllers/MemoListVC.swift
+++ MyNote/Controllers/MemoListVC.swift
@@ -35,6 +35,27 @@ class MemoListVC: UITableViewController {
     // 테이블 행을 구성하는 메소드.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        // 1. memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        // 2. 이미지속성이 비어있을 경우 "memoCell" 아니면 "memoCellWithImage"
+        let cellId = row.image == nil ? "memoCell" : "memoCellWithImage"
+        
+        // 3. 재사용 큐로부터 프로토타입셀의 인스턴스를 전달받는다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
+        // 4. memoCell의 내용을 구성한다.
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        // 5. Date 타입의 날짜를 포맷에 맞게 변경한다. ex) yyyy-mm-dd HH:mm:ss
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text = formatter.string(from: row.regdate!)
+        
+        // 6. cell 객체를 리턴한다.
+        return cell
         
         
         
